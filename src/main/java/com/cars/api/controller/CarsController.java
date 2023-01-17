64aa7885171261e15bf680884b3cb1be62b1e464
luiz <lguilhermefl@gmail.com>
2023-01-17 12:37:14 -0300

    feat: create cars dto

--- src/main/java/com/cars/api/controller/CarsController.java
+++ src/main/java/com/cars/api/controller/CarsController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cars.api.dto.CarDTO;
+
 @RestController
 @RequestMapping("/api/cars")
 public class CarsController {
 
     @PostMapping
-    public void create(@RequestBody String req) {
+    public void create(@RequestBody CarDTO req) {
         System.out.println(req);
     }
 }
